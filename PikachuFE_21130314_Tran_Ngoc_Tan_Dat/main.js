d3c5687f76b72b537a0bfbdcdeea79f076b407a0
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2024-04-17 14:21:03 +0700

    update file new

--- PikachuFE_21130314_Tran_Ngoc_Tan_Dat/main.js
+++ PikachuFE_21130314_Tran_Ngoc_Tan_Dat/main.js
@@ -4,6 +4,10 @@ let gameWidth = 11;
 let gameHeight = 18;
 let totalCells = (gameHeight * gameWidth);
 var DONE = false
+let shuffleIndex
+let shuffleTimes = [25, 20, 18, 15, 12, 10]
+let timeDuration = [5000, 4500, 4000, 3500, 3000, 2500]
+let timeIndex
 
 let wrapper = document.querySelector(".wrapper")
 let title = document.querySelector(".title")
@@ -14,9 +18,14 @@ let intro = document.querySelector(".intro")
 let intro_content = document.querySelector(".intro__content")
 let content__levels = document.querySelector(".content__levels")
 let start_btn = document.querySelector(".start_btn")
+let level_select = document.getElementById("levels")
+let level = document.getElementById("level_index")
+let shuffle = document.getElementById("shuffle_index")
 
 let main_game = document.querySelector(".main__game")
 let start__game = document.querySelector(".start__game")
+
+let game__content_right = document.querySelector(".game__content--right")
 
 
 
@@ -58,17 +67,141 @@ const imagePaths = [
     "./Images/34.png",
     "./Images/35.png",
     "./Images/36.png",
+    
 
     // Thêm các đường dẫn hình ảnh khác tại đây nếu cần
 ];
 
+const themeAudio = new Audio()
+themeAudio.src = "./audio/theme.mp3"
+themeAudio.volume = 0.8
+
+const scoreAudio = new Audio()
+scoreAudio.src = "./audio/connect(1).mp3"
+scoreAudio.volume = 0.8
 
 
 start_btn.onclick = function () {
     overview.style.display = "none"
     main_game.style.display = "block"
     init();
+    // timer_bar.style.display = "inline-flex"
+    // labels.style.display = "inline-flex"
+    // score_label.style.display = "block"
+    // content.style.display = "flex"
+    clearInterval(countDown)
+    timer.style.width = "300px"
+    interval = 100
+    countDown = null
+    levelController()
+    shuffle.innerHTML = shuffleIndex
+    themeAudio.play()
+    level.innerHTML = level_select.options[level_select.selectedIndex].text
+
 }
+
+
+//level controller
+const levelController = () => {
+    let level_info = level_select.options[level_select.selectedIndex].text
+    switch (level_info) {
+        case 'EASY':
+            timeIndex = timeDuration[0]
+            shuffleIndex = shuffleTimes[0]
+            break
+        case 'MEDIUM':
+            timeIndex = timeDuration[1]
+            shuffleIndex = shuffleTimes[1]
+            break
+        case 'HARD':
+            timeIndex = timeDuration[2]
+            shuffleIndex = shuffleTimes[2]
+            break
+        case 'EXTREM':
+            timeIndex = timeDuration[3]
+            shuffleIndex = shuffleTimes[3]
+            break
+        case 'TERRIBLE':
+            timeIndex = timeDuration[4]
+            shuffleIndex = shuffleTimes[4]
+            break
+        case 'NIGHTMARE':
+            timeIndex = timeDuration[5]
+            shuffleIndex = shuffleTimes[5]
+            break
+    }
+}
+
+//return to menu button
+const returnToMenuButton = () => {
+    location.reload()
+}
+
+const retryThisLevel = () => {
+    gameFrame.innerHTML = ""
+    end_frame.style.display = "none"
+    gameFrame.style.display = "block"
+    timer_bar.style.display = "inline-flex"
+    labels.style.display = "inline-flex"
+    levelController()
+    init()
+
+}
+
+// timer countdown bar
+let timer_bar = document.createElement("div")
+timer_bar.className = "timer_bar"
+let labels = document.createElement("div")
+labels.className = "labels"
+const timerBar = () => {
+    
+    //timer
+    let timer_wrapper = document.createElement("div")
+    timer_wrapper.className = "timer_wrapper"
+    let timer = document.createElement("div")
+    timer.id = "timer_id"
+    timer.className = "timer"
+
+    //pause button
+    let pause = document.createElement("div")
+    pause.onclick = function () {
+        pauseButton()
+    }
+    pause.className = "pause"
+    pause.innerHTML = "<i class=\"fa-solid fa-pause\"></i>"
+
+    //restart game button
+    let restart = document.createElement("div")
+    restart.className = "restart"
+    restart.innerHTML = "<i class=\"fa-solid fa-repeat\"></i>"
+    restart.onclick = function () {
+        shuffleButton()
+    }
+    //label
+    // labels.style.display = "none"
+    let pause_label = document.createElement("div")
+    pause_label.className = "pause_label"
+    pause_label.innerHTML = "Stop"
+    let restart_label = document.createElement("div")
+
+    restart_label.className = "restart_label"
+    restart_label.innerHTML = "Shuffle"
+    let timer_label = document.createElement("div")
+    timer_label.className = "timer_label"
+    timer_label.innerHTML = "Timer"
+
+    game__content_right.append(timer_bar)
+    timer_bar.append(pause)
+    timer_bar.append(restart)
+    timer_bar.append(timer_wrapper)
+    timer_wrapper.append(timer)
+
+    game__content_right.append(labels)
+    labels.append(pause_label)
+    labels.append(restart_label)
+    labels.append(timer_label)
+}
+timerBar()
 
 //Tạo mảng cells thêm ảnh vào mảng 
 var cells = []
@@ -94,17 +227,6 @@ function shuffleCellImages(cellImages) {
     return cellImages;
 }
 
-// function shuffleCellImages(cellImages) {
-//     for (let i = 0; i < cellImages.length; i++) {
-//         for (var j = 0; j < 4; j++) {
-//             let j = Math.floor(Math.random() * (i + 1));
-//             [cellImages[i], cellImages[j]] = [cellImages[j], cellImages[i]];
-//         }
-//     }
-//     return cellImages;
-// }
-
-
 //import images to game frame
 function init() {
     for (let i = 0; i < gameWidth; i++) {
@@ -129,6 +251,7 @@ function init() {
             draw(i, j, cells[i][j])
         }
     }
+    console.log(cells.length)
 }
 
 //update the game frame after changes
@@ -147,10 +270,12 @@ const draw = (x, y, value) => {
     cell.className = "cell cell_" + x + "-" + y;
     cell.style.backgroundImage = `url(${value})`;
     if (value === 0) {
-        cell.style.backgroundColor = "white"
+        cell.style.backgroundColor = " rgb(255, 170, 66);"
         cell.style.border = "none"
         
+        
     }
+    cell.style.padding = "1px "
     cell.style.left = `${y * cellWidth}px`;
     cell.style.top = `${x * cellHeight}px`;
     cell.addEventListener("click", (e) => {
@@ -180,25 +305,100 @@ let interval_status = false
 let countDown = null
 let countdown_value
 
-function mouseClicked(x, y, e) {
-    // if (countDown === null) {
-    //     countDown = setInterval(() => {
-    //         interval--
+let shuffle_display
+const shuffleButton = () => {
+    if (shuffleIndex >= 0) {
+        for (let i = 1; i < cells.length - 1; i++) {
+            for (let j = 1; j < cells[i].length - 1; j++) {
+                if (cells[i][j] !== 0) {
+                    let c1 = cells[i][j]
+                    let i_r = Math.floor(Math.random() * (gameHeight - 8)) + 1
+                    let j_r = Math.floor(Math.random() * (gameWidth - 8)) + 1
+                    for (let k = 0; k < 10000; k++) {
+                        if (cells[i_r][j_r] === 0) {
+                            i_r = Math.floor(Math.random() * (gameHeight - 8)) + 1
+                            j_r = Math.floor(Math.random() * (gameWidth - 8)) + 1
+                        } else {
+                            break
+                        }
+                    }
+                    let value_c1 = cells[i][j]
+                    cells[i][j] = cells[i_r][j_r]
+                    cells[i_r][j_r] = value_c1
+                }
+            }
+        }
+        shuffleIndex = shuffleIndex - 1
+        shuffle_display = shuffleIndex
+        updateShuffle(shuffle_display)
+    }
+    reRender()
+}
 
-    //         let progressWidth = interval - (interval * 0.0000000025)
-    //         if (interval > 0) {
-    //             timer.style.width = progressWidth + "%"
-    //             interval_update = progressWidth
-    //         } else {
-    //             clearInterval(countDown)
-    //             timer.style.width = "0%"
-    //             let alert = `<div class="time_out">
-    //                             <p>TIME OUT!</p>
-    //                          </div>`
-    //             start__game.innerHTML += alert
-    //         }
-    //     }, timeIndex)
-    // }
+let timer = document.getElementById("timer_id")
+const pauseButton = () => {
+    let width_stopped = interval_update
+    if (!interval_status) {
+        interval_status = true
+        timer.style.width = width_stopped + "%"
+        clearInterval(countDown)
+        countDown = null
+        let pause = document.getElementsByClassName("pause")[0]
+        let text = document.getElementsByClassName("pause_label")[0]
+        text.textContent = "Play"
+        start__game.style.backgroundImage = "url(\"image/pokestop.jpg\")"
+        start__game.style.opacity = "0.05"
+        pause.innerHTML = "<i class=\"fa-solid fa-play\"></i>"
+    } else {
+        interval_status = false
+        let pause = document.getElementsByClassName("pause")[0]
+        start__game.style.opacity = "1"
+        start__game.style.backgroundImage = "none"
+        pause.innerHTML = "<i class=\"fa-solid fa-pause\"></i>"
+        let text = document.getElementsByClassName("pause_label")[0]
+        text.textContent = "Stop"
+        countDown = setInterval(() => {
+            interval--
+            let timer = document.getElementById("timer_id")
+            let progressWidth = interval
+
+            if (interval > 0) {
+                timer.style.width = progressWidth + "%"
+                interval_update = progressWidth
+
+            } else {
+                clearInterval(countDown)
+                timer.style.width = "0%"
+                let alert = `<div class="time_out">
+                                <p>TIME OUT!</p>
+                             </div>                
+                `
+                start__game.innerHTML += alert
+            }
+        }, timeIndex)
+    }
+    countdown_value = countDown
+}
+
+function mouseClicked(x, y, e) {
+    if (countDown === null) {
+        countDown = setInterval(() => {
+            interval--
+
+            let progressWidth = interval - (interval * 0.0000000025)
+            if (interval > 0) {
+                timer.style.width = progressWidth + "%"
+                interval_update = progressWidth
+            } else {
+                clearInterval(countDown)
+                timer.style.width = "0%"
+                let alert = `<div class="time_out">
+                                <p>TIME OUT!</p>
+                             </div>`
+                start__game.innerHTML += alert
+            }
+        }, timeIndex)
+    }
 
     let count = 0
     if (cells[x][y] !== 0) {
@@ -235,17 +435,29 @@ function mouseClicked(x, y, e) {
     }
 }
 
+//shuffle times update
+const updateShuffle = (shuffle_display) => {
+    if (shuffle_display < 0) { return }
+    shuffle_display = shuffleIndex
+    // shuffleIndex = shuffle_times
+    shuffle.innerHTML = shuffleIndex
+}
+
+const decreaseShuffle = () => {
+    updateShuffle(shuffleIndex - 1)
+}
+
 // score update
 let score = 0
-// const updateScore = newScore => {
-//     if (isNaN(newScore)) return
-//     score = newScore
-//     score_title.innerHTML = score
-// }
+const updateScore = newScore => {
+    if (isNaN(newScore)) return
+    score = newScore
+    score_title.innerHTML = score
+}
 
-// const increaseScore = () => {
-//     updateScore(score + 100)
-// }
+const increaseScore = () => {
+    updateScore(score + 100)
+}
 
 let count = 0;
 const findWay = (cellStart, cellTarget) => {
@@ -331,7 +543,7 @@ const logPath = (v) => {
             }
         }
         console.log("Count: ", count)
-        if (count <= 3 && !finishPath) {
+        if (count <= 2 && !finishPath) {
             drawPathWay(findWay, path, cellTarget)
         }
     }
@@ -443,14 +655,29 @@ const drawPathWay = (path, arrr, cellTarget) => {
         }
 
     }
-    // increaseScore()
+    increaseScore()
     setTimeout(() => {
         arrr.map(j => {
             cells[j[0]][j[1]] = 0
         })
-        // scoreAudio.play()   
+        scoreAudio.play()   
         reRender()
         // levelDirection()
         // endGame()
     }, 400)
 }
+
+//swap 2 cells in same row by x index
+const swapCellX = (i, index, x) => {
+    let tmp = cells[x][i]
+    cells[x][i] = cells[x][index]
+    cells[x][index] = tmp
+}
+
+//swap 2 cells in same column by y index
+const swapCellY = (i, index, y) => {
+    let tmp = cells[i][y]
+    cells[i][y] = cells[index][y]
+    cells[index][y] = tmp
+}
+
